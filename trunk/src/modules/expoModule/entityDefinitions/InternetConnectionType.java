ec17a98b62bda7b20b1fa469c976f36cc75ab916
glacasse <glacasse@9a820410-0acf-11df-9f86-67d497c0387b>
2010-02-02 19:07:06 +0000

    Enlevé quelques warnings

--- trunk/src/modules/expoModule/entityDefinitions/InternetConnectionType.java
+++ trunk/src/modules/expoModule/entityDefinitions/InternetConnectionType.java
@@ -1,10 +1,12 @@
 package modules.expoModule.entityDefinitions;
 
-import newtonERP.orm.field.*;
-import newtonERP.orm.field.type.*;import newtonERP.orm.associations.AccessorManager;
-import newtonERP.module.AbstractOrmEntity;
-import java.util.Hashtable;
 import java.util.Vector;
+
+import newtonERP.module.AbstractOrmEntity;
+import newtonERP.orm.field.Field;
+import newtonERP.orm.field.Fields;
+import newtonERP.orm.field.type.FieldInt;
+import newtonERP.orm.field.type.FieldString;
 
 /**
  * Connexion internet
@@ -18,19 +20,20 @@ public class InternetConnectionType extends AbstractOrmEntity
      */
     public InternetConnectionType() throws Exception
     {
-        super();
-        setVisibleName("Connexion internet");
+	super();
+	setVisibleName("Connexion internet");
     }
 
     public Fields initFields() throws Exception
     {
-        Vector<Field<?>> fieldList = new Vector<Field<?>>();
+	Vector<Field<?>> fieldList = new Vector<Field<?>>();
 
-        FieldInt pKinternetConnectionTypeID = new FieldInt("Numéro", getPrimaryKeyName());
-        fieldList.add(pKinternetConnectionTypeID);
+	FieldInt pKinternetConnectionTypeID = new FieldInt("Numéro",
+		getPrimaryKeyName());
+	fieldList.add(pKinternetConnectionTypeID);
 
-        FieldString name = new FieldString("Description", "Name");
-        fieldList.add(name);
-        return new Fields(fieldList);
+	FieldString name = new FieldString("Description", "Name");
+	fieldList.add(name);
+	return new Fields(fieldList);
     }
 }
