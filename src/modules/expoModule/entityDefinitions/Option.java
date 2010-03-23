cce0888897b1d34fabb2fd9f860a244ca9483c6a
glacasse <glacasse@9a820410-0acf-11df-9f86-67d497c0387b>
2010-03-23 19:15:53 +0000

    Ajout de champ calculé dans factures

--- src/modules/expoModule/entityDefinitions/Option.java
+++ src/modules/expoModule/entityDefinitions/Option.java
@@ -6,6 +6,7 @@ import newtonERP.module.AbstractOrmEntity;
 import newtonERP.orm.field.Field;
 import newtonERP.orm.field.Fields;
 import newtonERP.orm.field.type.FieldCurrency;
+import newtonERP.orm.field.type.FieldDouble;
 import newtonERP.orm.field.type.FieldInt;
 import newtonERP.orm.field.type.FieldString;
 
@@ -33,10 +34,22 @@ public class Option extends AbstractOrmEntity
 	fieldList.add(pKoptionID);
 
 	FieldString name = new FieldString("Description", "name");
+	name.setNaturalKey(true);
 	fieldList.add(name);
 
 	FieldCurrency prix = new FieldCurrency("Prix", "price");
+	prix.setNaturalKey(true);
 	fieldList.add(prix);
 	return new Fields(fieldList);
+    }
+
+    /**
+     * @return prix de l'option
+     */
+    public double getPrice()
+    {
+	FieldDouble fieldDoublePrice = (FieldDouble) getFields().getField(
+		"price");
+	return fieldDoublePrice.getData();
     }
 }
