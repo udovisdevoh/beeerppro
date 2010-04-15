9312f51893e2610ea738355be07f2119be16340e
glacasse <glacasse@9a820410-0acf-11df-9f86-67d497c0387b>
2010-04-15 23:02:59 +0000

    Lorsque membre paie facture, zones concernées sont activées

--- trunk/src/modules/expoModule/actions/PayInvoice.java
+++ trunk/src/modules/expoModule/actions/PayInvoice.java
@@ -5,6 +5,8 @@ import java.util.Hashtable;
 import modules.expoModule.entityDefinitions.KioskInvoice;
 import newtonERP.module.AbstractAction;
 import newtonERP.module.AbstractEntity;
+import newtonERP.module.AbstractOrmEntity;
+import newtonERP.orm.associations.PluralAccessor;
 
 /**
  * Action de payer une facture et d'effectuer les changements concernés par
@@ -30,6 +32,25 @@ public class PayInvoice extends AbstractAction
 	invoice.setData("isPaid", true);
 	invoice.save();
 
+	activateZones(invoice);
+
 	return invoice.getUI(parameters);
+    }
+
+    private void activateZones(KioskInvoice invoice) throws Exception
+    {
+	PluralAccessor invoiceItemList = invoice
+		.getPluralAccessor("KioskInvoiceItem");
+
+	for (AbstractOrmEntity invoiceItem : invoiceItemList)
+	{
+	    PluralAccessor zoneList = invoiceItem.getPluralAccessor("Zone");
+
+	    for (AbstractOrmEntity zone : zoneList)
+	    {
+		zone.setData("isActive", true);
+		zone.save();
+	    }
+	}
     }
 }
